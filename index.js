3d099749f3aca249c39c9f53f01755a0679acbaf
Grant Cummings <grantcummings4429@gmail.com>
2024-04-11 14:05:04 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  reversedString = word.split('').reverse().join('')
+  return word === reversedString ? true : false
+  
 }
 
 /* 
   Add your pseudocode here
+  The function takes a string input of lower case letters 
+  the code will reaarange the string input and make a backwards version 
+  example: robot will become tobor
+  if the reaaranged word is exactly the same as the the initial string input
+  then the function will return a boolean of true
+  if it is false then it will return the boolean false
 */
 
 /*
   Add written explanation of your solution here
+  racecar will be true
+  robot will be false
 */
 
 // You can run `node index.js` to view these console logs
